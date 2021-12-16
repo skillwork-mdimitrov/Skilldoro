a85f0f19a4783b4647e5bec2842e2328f155f4d7
Maksim1337 <maksimdimitrovmail@gmail.com>
2021-12-16 16:13:33 +0200

    Main page design static

---
+++ src/Components/Header.jsx
@@ -0,0 +1,38 @@
+import Button from 'react-bootstrap/Button';
+import { ImUpload } from 'react-icons/im';
+import {Breadcrumb} from "react-bootstrap";
+
+const Header = () => {
+
+  return (
+    <header className="d-flex justify-content-around align-items-center m-2 mb-3">
+      <div className="points-container">
+        <div className="d-flex align-items-center">
+          <span>Points: 2000 - </span>
+          <Button variant="light"
+                  className="d-inline-flex align-items-center gap-2 p-1 border-dark">
+            <span>Upload</span>
+            <ImUpload />
+          </Button>
+        </div>
+      </div>
+      <h1>Skilldoro™</h1>
+
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Check points</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Rewards</Breadcrumb.Item>
+      </Breadcrumb>
+    </header>
+  )
+}
+
+export default Header;
+
+/*
+  header
+    div.points-container
+    h1.skilldoro
+    <Tabs>
+      <Tab/> * 3
+ */
---
+++ src/Pages/Skilldoro.jsx
@@ -0,0 +1,10 @@
+import Header from "../Components/Header"
+import Main from "../Components/Main";
+
+const Skilldoro = () =>
+  <>
+    <Header />
+    <Main/>
+  </>
+
+export default Skilldoro;
