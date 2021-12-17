e9be635a646e34c113b9c9c45e554f44a3bb1041
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-12-17 14:33:55 +0200

    Styling for CheckPoints and Rewards pages

--- src/Pages/CheckPoints.jsx
+++ src/Pages/CheckPoints.jsx
@@ -1,8 +1,18 @@
 import Header from "../Components/Header";
 import {Button} from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledCheckPoints = styled.div`
+  background-image: url("https://img5.goodfon.com/wallpaper/nbig/c/f3/synth-retrowave-synthwave-new-retro-wave-sintveiv-retrove-14.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-position-y: top;
+  min-height: 100vh;
+`;
 
 const CheckPoints = ({pointsForTheDay}) => (
-  <>
+  <StyledCheckPoints>
     <Header pointsForTheDay={pointsForTheDay}/>
     <div className="check-points d-flex flex-column align-items-center">
         <div className="d-flex my-4">
@@ -12,6 +22,6 @@ const CheckPoints = ({pointsForTheDay}) => (
 
         <span>Hello Peter, your points are: <strong>7227</strong></span>
     </div>
-  </>
+  </StyledCheckPoints>
 );
 export default CheckPoints;
