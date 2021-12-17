4a5f26a2e77b9ed3ec914c90a59a9c1e40aa442b
IvanSolenkov <ivan_solenkov1999@abv.bg>
2021-12-17 17:30:25 +0200

    Added new Rewards and fixed responsive design

--- src/Components/SingleReward.jsx
+++ src/Components/SingleReward.jsx
@@ -1,7 +1,18 @@
 import {Button, Card} from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+  min-width: 15rem;
+  width: 20rem;
+  flex-basis: 20%;
+
+  @media only screen and (max-width: 1400px) {
+    flex-basis: 25%;
+  }
+`;
 
 const SingleReward = ({rewardImage, rewardName, rewardPoints, rewardDescription, onClick }) =>
-    <Card className="mt-4" style={{ width: '20rem' }}>
+    <StyledCard className="mt-4 text-black">
         <Card.Img variant="top" src={rewardImage} style={{height: '200px'}} />
         <Card.Body className="d-flex flex-column align-items-center">
             <Card.Title>{rewardName}</Card.Title>
@@ -9,8 +20,11 @@ const SingleReward = ({rewardImage, rewardName, rewardPoints, rewardDescription,
             <Card.Text>
                 {rewardDescription}
             </Card.Text>
-            <Button onClick={onClick} variant="success" className="text-uppercase fw-bold px-5">Get</Button>
+
         </Card.Body>
-    </Card>
+        <Card.Footer className="text-center border-0 bg-white">
+            <Button onClick={onClick} variant="success" className="text-uppercase fw-bold px-5">Get</Button>
+        </Card.Footer>
+    </StyledCard>
 
 export default SingleReward;
